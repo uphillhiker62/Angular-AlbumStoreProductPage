a9662779a86d7b51e1cf32cd3e3e9e3fa2f381c2
Cody Gates <cody.gates.89@gmail.com>
2018-08-03 23:16:25 -0600

    section 3 complete

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+
+  constructor(private _http: Http) {
+   }
+
+   getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json());
+  }
+
+}
